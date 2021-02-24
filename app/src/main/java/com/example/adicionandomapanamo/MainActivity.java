52ade2812b158483c3a74d31b39e6eb3ff090c36
kleberfort <kleberfort@gmail.com>
2021-02-23 22:49:34 -0300

    adicionado alguns códigos nos arquivos: Manifest, activity_main.xml, MainActitivity.java, build.gradle(app), e criado o arquivo map_api.xml que vai ter a minha chave de API

--- app/src/main/java/com/example/adicionandomapanamo/MainActivity.java
+++ app/src/main/java/com/example/adicionandomapanamo/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.adicionandomapanamo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.textclassifier.TextClassifierEvent;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+    map = googleMap;
+
+    LatLng basePoliciaJoseEuclides = new LatLng(-3.8512687878753002, -38.5186557826621);
+    map.addMarker(new MarkerOptions().position(basePoliciaJoseEuclides).title("Base da policia"));
+    map.moveCamera(CameraUpdateFactory.newLatLng(basePoliciaJoseEuclides));
     }
 }
