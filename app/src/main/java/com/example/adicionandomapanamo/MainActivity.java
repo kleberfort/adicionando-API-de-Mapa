cb8be5952d56a17bba662fa625319246d9946333
kleberfort <kleberfort@gmail.com>
2021-02-27 23:11:13 -0300

    Inserido os códigos para recuperar a localização do usuário pegando o serviço de gps, adicionado a classe Permissao necessária para a nossa aplicação

--- app/src/main/java/com/example/adicionandomapanamo/MainActivity.java
+++ app/src/main/java/com/example/adicionandomapanamo/MainActivity.java
@@ -1,26 +1,52 @@
 package com.example.adicionandomapanamo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.textclassifier.TextClassifierEvent;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    private String[] permissoes = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //validar as permissões
+        Permissoes.validarPermissoes(permissoes, this, 1);
+
+
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -30,10 +56,120 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-    map = googleMap;
+        map = googleMap;
 
-    LatLng basePoliciaJoseEuclides = new LatLng(-3.8512687878753002, -38.5186557826621);
-    map.addMarker(new MarkerOptions().position(basePoliciaJoseEuclides).title("Base da policia"));
-    map.moveCamera(CameraUpdateFactory.newLatLng(basePoliciaJoseEuclides));
+        // objeto responsável por gerenciar a localização do usuário
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Localização", "onLocationChanged" + location.toString());
+
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+
+                map.clear();
+                LatLng localUsuario = new LatLng(latitude, longitude);
+                map.addMarker(new MarkerOptions().position(localUsuario).title("Meu local"));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 16));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    15000,
+                    10,
+                    locationListener
+            );
+        }
+/*
+        LatLng basePoliciaJoseEuclides = new LatLng(-3.8512687878753002, -38.5186557826621);
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                double latitude = latLng.latitude;
+                double longitude = latLng.longitude;
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        "onClick Lat: " + latitude + " Long" + longitude,
+                        Toast.LENGTH_LONG
+                ).show();
+
+                map.addMarker(
+                        new MarkerOptions()
+                                .position(latLng)
+                                .title("Local")
+                                .snippet("Adicionado o marcador no click do mapa")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
+                );
+
+            }
+        });
+
+       */
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                //alerta
+                alertaValidarPermissao();
+            } else if (permissaoResultado == PackageManager.PERMISSION_GRANTED) {
+                //recuperar localização do usuário
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            15000,
+                            10,
+                            locationListener
+                    );
+                }
+
+            }
+
+        }
+    } // onRequestPermissionResult
+
+    private void alertaValidarPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
